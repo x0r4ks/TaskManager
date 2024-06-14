94edf28ad77aac01532c65e2f90f74338d8c3f27
x0r4ks <max1500demo@gmail.com>
2024-06-14 12:32:27 +0300

    <feat>: single process done

---
+++ Process/Program.cs
@@ -0,0 +1,62 @@
+﻿#define SINGLE_PROCESS
+#define MULTIPLE_PROCESS
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace Process
+{
+	internal class Program
+	{
+
+		static string getUsetName(IntPtr process_handle)
+		{
+			IntPtr handle = IntPtr.Zero;
+			OpenProcessToken(process_handle, 8, out handle);
+			System.Security.Principal.WindowsIdentity wi = new System.Security.Principal.WindowsIdentity(handle);
+			string name = wi.Name;
+			CloseHandle(handle);
+			return name;
+		}
+
+		static void Main(string[] args)
+		{
+#if SIGLE_PROCESS
+			Console.Write("Введите имя программы: ");
+			string process_name = Console.ReadLine();
+			System.Diagnostics.Process process = new System.Diagnostics.Process();
+			process.StartInfo.FileName = process_name;
+			process.Start();
+
+
+
+			Console.WriteLine(process.Id);
+
+			Console.WriteLine(process.ProcessName);
+			Console.WriteLine($"User name: {getUsetName(process.Handle)}");
+			Console.WriteLine($"SessionID: {process.SessionId}");
+			Console.WriteLine($"Threads: {process.Threads.Count}");
+			Console.WriteLine($"Priority Class: {process.PriorityClass}");
+#endif
+
+	
+
+
+		}
+
+		[DllImport("advapi32.dll", SetLastError = true)]
+		private static extern bool OpenProcessToken(IntPtr processHandle, 
+			uint desiredAcsess, out IntPtr handle);
+
+		[DllImport("kernel32", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool CloseHandle(IntPtr handle);
+	}
+}
--- TaskManager/Form1.Designer.cs
+++ TaskManager/Form1.Designer.cs
@@ -29,21 +29,91 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPageProcesses = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.listView_Processes = new System.Windows.Forms.ListView();
+            this.tabControl.SuspendLayout();
+            this.tabPageProcesses.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Controls.Add(this.tabPageProcesses);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(704, 416);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabPageProcesses
+            // 
+            this.tabPageProcesses.Controls.Add(this.listView_Processes);
+            this.tabPageProcesses.Location = new System.Drawing.Point(4, 22);
+            this.tabPageProcesses.Name = "tabPageProcesses";
+            this.tabPageProcesses.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageProcesses.Size = new System.Drawing.Size(696, 390);
+            this.tabPageProcesses.TabIndex = 0;
+            this.tabPageProcesses.Text = "Processos";
+            this.tabPageProcesses.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Location = new System.Drawing.Point(0, 419);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(704, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // listView_Processes
+            // 
+            this.listView_Processes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView_Processes.HideSelection = false;
+            this.listView_Processes.Location = new System.Drawing.Point(3, 3);
+            this.listView_Processes.Name = "listView_Processes";
+            this.listView_Processes.Size = new System.Drawing.Size(690, 384);
+            this.listView_Processes.TabIndex = 0;
+            this.listView_Processes.UseCompatibleStateImageBehavior = false;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.tabControl);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "TaskManager";
+            this.tabControl.ResumeLayout(false);
+            this.tabPageProcesses.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPageProcesses;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ListView listView_Processes;
     }
 }
 
